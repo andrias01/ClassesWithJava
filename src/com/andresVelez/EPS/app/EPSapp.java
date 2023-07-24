835c4b6c3257a78de1f23c1c9feacaf46430dae4
andrias01 <103292498+andrias01@users.noreply.github.com>
2023-07-24 15:48:28 -0500

    Add files via upload

---
+++ src/com/andresVelez/EPS/app/EPSapp.java
@@ -0,0 +1,14 @@
+package EPS.app;
+
+import EPS.dominio.EPS;
+import EPS.dominio.Enfermedad;
+import EPS.dominio.Paciente;
+
+import java.util.List;
+
+public class EPSapp {
+    public static void main(String[] args) {
+
+
+    }
+}
